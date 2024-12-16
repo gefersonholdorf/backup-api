63d47ab010d605d95f09ace2450a9ac00c2e7e58
gefersonholdorf <gefersonholdorf88@gmail.com>
2024-12-16 09:39:26 -0300

    Correção feita para pegar os jsons do repositorio

--- src/backup/backup.service.ts
+++ src/backup/backup.service.ts
@@ -19,17 +19,18 @@ export class BackupService {
     private dateActual = this.dataFormat(new Date())
 
     private readonly nameSystemArquives = [
-        `diamante_${this.dateActual}.json`, 
+        `diamante_${this.dateActual}.json`,
         `bot-george_${this.dateActual}.json`,
         `DOESC_${this.dateActual}.json`,
-        `BILLER-CENTRAIS_${this.dateActual}.json`,
-        `BILLER-BD_${this.dateActual}.json`
+        `biller-gov_${this.dateActual}.json`,
+        `BILLER-BD_${this.dateActual}.json`,
     ]
 
     @Cron('*/1 * * * *')
     async getJsonData() {
         console.log('Cron executada')
         for (let file of this.nameSystemArquives) {
+            console.log(file)
             try {
                 const response = await lastValueFrom(
                     this.httpService.get(`${process.env.URL_STORAGE}/${file}`)
@@ -40,20 +41,22 @@ export class BackupService {
                     continue
                 }
                 await this.create(response.data)
+
                 continue
 
             } catch (error) {
                 try {
                     const name = file.split('_')[0]
-                    const newDate = this.dataFormat(new Date(this.dateActual))
 
                     const isInsert = await this.verifyInsert(name, 0)
                     if (isInsert) {
-                        continue
+                        return
                     }
+
+                    const nameFormat = name.toUpperCase()
                     const backup = await this.backupRepository.create({
-                        name,
-                        date: new Date(newDate),
+                        name: nameFormat,
+                        date: this.dateActual,
                         status: 0
                     })
 
@@ -67,22 +70,25 @@ export class BackupService {
     }
 
     async create(createBackupDTO : CreateBackupDTO) {
-        createBackupDTO.name = createBackupDTO.name.toUpperCase()
+        const name = createBackupDTO.name.toUpperCase()
 
         const isInsert = await this.verifyInsert(createBackupDTO.name, createBackupDTO.status)
+        console.log(isInsert)
         if (isInsert) {
             return
         }
-
-        const newDate = this.dataFormat(new Date(createBackupDTO.date))
+        const newDate = createBackupDTO.date
         
         try {
              const backup = await this.backupRepository.create({
                 ...createBackupDTO,
-                 date: newDate
+                name, 
+                date: newDate
              })
     
-             this.backupRepository.save(backup)
+             await this.backupRepository.save(backup)
+
+             return 
 
          } catch (error) {
              throw new BadRequestException("Erro ao criar novo registro do backup")
@@ -116,8 +122,19 @@ export class BackupService {
         return newBackups
     }
 
-    dataFormat(date : Date) {
-        return `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2, '0')}-${date.getFullYear()}`
+    dataFormat(date: Date): string {
+        const options: Intl.DateTimeFormatOptions = {
+            timeZone: 'America/Sao_Paulo',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+        };
+    
+        const formatter = new Intl.DateTimeFormat('pt-BR', options);
+        const formatted = formatter.format(date);
+    
+        const [day, month, year] = formatted.split('/');
+        return `${year}-${month}-${day}`;
     }
 
     dataFormatDatabase(date : Date) {
@@ -126,8 +143,8 @@ export class BackupService {
 
     async verifyInsert(name : string, status : number) {
         const backups = await this.backupRepository.createQueryBuilder('bkp')
-            .where('bkp.name LIKE :name', {name: `%${name}%`})
-            .andWhere('bkp.date = :date', {date: `${this.dataFormatDatabase(new Date(this.dateActual))}`})
+            .where('bkp.name LIKE :name', {name: `%${name.toUpperCase()}%`})
+            .andWhere('DATE(bkp.date) = :date', {date: `${this.dateActual}`})
             .getMany()
 
         if (backups.length == 1) {
